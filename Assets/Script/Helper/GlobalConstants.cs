dad1c9e7cac2e2543098771b7f45e381ac703ab8
Balaji <light.cyan00f@gmail.com>
2024-02-12 16:59:58 +0530

    Feature : Helper class

---
+++ Assets/Script/Helper/GlobalConstants.cs
@@ -0,0 +1,18 @@
+
+namespace CS.Helper
+{
+    public class GlobalConstants
+    {
+        public static bool IsAccel;
+        public static bool IsBreak;
+        public static bool IsTurningLeft;
+        public static bool IsTurningRight;
+        public static float Acceleration;
+        public static float Steering;
+        public static float AccelValue = 1f;
+        public const float MaxSteerAngle = 30f;
+        public const float BreakForce = 30000f;
+        public const float MotorForce = 100000f;
+    }
+    
+}
